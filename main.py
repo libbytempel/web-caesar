b893f5458af2cf662850ed45195e7077a8fcac0d
lst <libbytempel@elizabeths-mbp.attlocal.net>
2017-09-25 11:09:52 -0500

    encrypted message now showing in the form

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -31,7 +31,7 @@ form = """
             Rotate by:
             <input type="text" name="rot" value="0"/><br>
             </label>
-            <textarea type="text" name="text"/></textarea><br>
+            <textarea type="text" name="text"/>{0}</textarea><br>
             <input type="submit" value="Submit Query"/>
         </form>
     </body>
@@ -40,14 +40,14 @@ form = """
 
 @app.route("/")
 def index():
-    return form
+    return form.format('')
 
 @app.route("/add", methods=['POST'])
 def encrypt():
     text = str(request.form['text'])
     rot = int(request.form['rot'])
     rotated=rotate_string(text,rot)
-    content = "<h1>" + rotated + "</h1>"
-    return content
+#    content = "<h1>" + rotated + "</h1>"
+    return form.format(rotated)
 
 app.run()
